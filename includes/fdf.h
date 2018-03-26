1db38af2de18001eaa0f12c50b72344d5f5c6bce
Andrii STADNIK <astadnik@student.unit.ua>
2018-03-26 19:41:07 +0300

    Implemented Bresenham algorithm

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: astadnik <astadnik@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 18:53:36 by astadnik          #+#    #+#             */
-/*   Updated: 2018/03/24 18:55:27 by astadnik         ###   ########.fr       */
+/*   Updated: 2018/03/26 19:31:18 by astadnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <fcntl.h>
 
 typedef struct s_data	t_data;
-typedef int			t_point;
+typedef struct s_point	t_point;
 
 /*
 * Offs is here to move the origin point
@@ -34,6 +34,13 @@ struct		s_data
 	int		offs[2];
 	int		move[4];
 	int		rotate[4];
+};
+
+struct		s_point
+{
+	int	x;
+	int	y;
+	int	z;
 };
 
 int		get_input(int ac, char *av, t_data *data);
--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: astadnik <astadnik@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 18:25:04 by astadnik          #+#    #+#             */
-/*   Updated: 2018/03/24 19:01:02 by astadnik         ###   ########.fr       */
+/*   Updated: 2018/03/26 19:40:16 by astadnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,52 @@ int			exit_c(void)
 	exit(1);
 }
 
+static void	draw_line(t_data data, t_point p1, t_point p2)
+{
+	int dx;
+	int	dy;
+	int	error;
+	int	diry;
+
+	dx = abs(p2.x - p1.x);
+	dy = abs(p2.y - p1.y);
+	error = 0;
+	diry = p2.y - p1.y;
+	if (diry > 0)
+		diry = 1;
+	if (diry < 0)
+		diry = -1;
+	while (p1.x < p2.x)
+	{
+		mlx_pixel_put(data.mlx, data.win, p1.x, p1.y, 0x00ffffff);
+		error = error + dy;
+		if (2 * error >= dx)
+		{
+			p1.y = p1.y + diry;
+			error = error - dx;
+		}
+		p1.x++;
+	}
+}
+
+/* static t_point	convert(int *p1, t_data *data) */
+/* { */
+/* 	t_point	rez; */
+
+/* 	ft_printf("{blue}p1[0] = %d, p1[1] = %d p1[2] = %d\noffs[0] = %d, offs[1] = %d, scale = %d{eoc}\n", p1[0], p1[1], p1[2], data->offs[0], data->offs[1], data->scale); */
+/* 	rez.x = (p1[0] + data->offs[0]) * data->scale; */
+/* 	rez.y = (p1[1] + data->offs[1]) * data->scale; */
+/* 	rez.z = p1[2] * data->scale; */
+/* 	return (rez); */
+/* } */
+
 static int	draw(t_data *data)
 {
 	int	i;
 	int	j;
 
 	i = -1;
+	draw_line(*data, (t_point){100, 100, 1}, (t_point){150, 500, 1});
 	while (++i < data->height)
 	{
 		j = -1;
@@ -36,9 +76,17 @@ static int	draw(t_data *data)
 	return (42);
 }
 
+static int	triggers(int key, void *param)
+{
+	param = NULL;
+	if (key == 53)
+		exit(1);
+	return (1);
+}
+
 void	hooks(t_data data)
 {
 	mlx_loop_hook(data.mlx, &draw, &data);
 	mlx_hook(data.win, 17, 1L << 17, exit_c, NULL);
-	/* mlx_loop_hook(g_mlx_ptr, &f, NULL); */
+	mlx_hook(data.win, 2, 5, triggers, NULL);
 }

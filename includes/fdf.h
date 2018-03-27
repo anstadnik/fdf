a25ddb0fbdb7a237615a8211a79e0265526f6703
Andrii STADNIK <astadnik@student.unit.ua>
2018-03-27 17:27:26 +0300

    Now it works even better

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: astadnik <astadnik@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/23 18:53:36 by astadnik          #+#    #+#             */
-/*   Updated: 2018/03/27 15:56:18 by astadnik         ###   ########.fr       */
+/*   Updated: 2018/03/27 16:39:51 by astadnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,30 @@ typedef struct s_point	t_point;
 */
 
 // Add max height and make gradient based on it
-struct		s_data
+struct					s_data
 {
-	void		*mlx;
-	void		*win;
-	intmax_t	width;
-	intmax_t	height;
-	intmax_t	altitude;
-	intmax_t	maltitude;
-	intmax_t	***field;
-	intmax_t	scale;
-	intmax_t	offs[2];
+	void				*mlx;
+	void				*win;
+	intmax_t			width;
+	intmax_t			height;
+	intmax_t			altitude;
+	intmax_t			maltitude;
+	intmax_t			***field;
+	intmax_t			scale;
+	intmax_t			offs[2];
 };
 
-struct		s_point
+struct					s_point
 {
-	int	x;
-	int	y;
-	int	z;
+	int					x;
+	int					y;
+	int					z;
 };
 
-int	draw(t_data *data);
-void	draw_line(t_data data, t_point p1, t_point p2);
-int		get_input(int ac, char *av, t_data *data);
-int		get_sizes_and_check(char *av, t_data *data, t_list **head);
-void	hooks(t_data data);
+int						draw(t_data *data);
+void					draw_line(t_data data, t_point p1, t_point p2);
+int						get_input(int ac, char *av, t_data *data);
+int						get_sizes_and_check(char *av, t_data *data, t_list **head);
+void					hooks(t_data data);
 
 #endif
--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: astadnik <astadnik@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 18:25:04 by astadnik          #+#    #+#             */
-/*   Updated: 2018/03/27 16:19:32 by astadnik         ###   ########.fr       */
+/*   Updated: 2018/03/27 17:26:25 by astadnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,9 @@ static t_point	convert(intmax_t *p1, t_data *data)
 {
 	t_point	rez;
 
-	/* ft_printf("p1[0] = %d, p1[1] = %d p1[2] = %d\noffs[0] = %d, offs[1] = %d, scale = %d\n", p1[0], p1[1], p1[2], data->offs[0], data->offs[1], data->scale); */
 	rez.z = p1[2] * data->altitude;
 	rez.x = p1[0] * data->scale - rez.z / 2 + data->offs[1];
 	rez.y = p1[1] * data->scale - 2 * rez.z / 3 + data->offs[0];
-	/* ft_printf("x = %d, y = %d, z = %d\n", rez.x, rez.y, rez.z); */
 	return (rez);
 }
 
@@ -43,19 +41,11 @@ int	draw(t_data *data)
 		while (++j < data->width)
 		{
 			if (i + 1 < data->height)
-			{
-				/* ft_printf("\np1.x = %d, p1.y = %d, p1.z = %d, p2.x = %d, p2.y = %d, p2.z = %d, i = %d, j = %d\n", data->field[i][j][0], data->field[i][j][1], data->field[i][j][2], data->field[i + 1][j][0], data->field[i + 1][j][1], data->field[i + 1][j][2], i, j); */
 				draw_line(*data, convert(data->field[i][j], data), convert(data->field[i + 1][j], data));
-			}
 			if (j + 1 < data->width)
 				draw_line(*data, convert(data->field[i][j], data), convert(data->field[i][j + 1], data));
 		}
-		/* mlx_pixel_put(data->mlx, data->win, */
-		/* 		data->field[i][j][0] + data->offs[0], // data->field[i][j][2], */
-		/* 		data->field[i][j][1] + data->offs[1], // data->field[i][j][2], */
-		/* 		0x00ffffff); */
 	}
-	/* ft_printf("hehey\n"); */
 	return (42);
 }
 
@@ -68,17 +58,22 @@ static int	triggers(int key, void *param)
 	if (key == 53)
 		exit(1);
 	else if (key == 126)
-		data->offs[0] -= 10;
+		data->offs[0] -= data->offs[0] < -100 ? 0 : 1;
 	else if (key == 125)
-		data->offs[0] += 10;
+		data->offs[0] += data->offs[0] > data->width * data->scale ? 0 : 1;
 	else if (key == 123)
-		data->offs[1] -= 10;
+		data->offs[1] = data->offs[1] < -100 ? 0 : -1;
 	else if (key == 124)
-		data->offs[1] += 10;
+		data->offs[1] += data->offs[1] > data->height * data->scale ? 0 : 1;
 	else if (key == 24)
-		data->altitude += 10;
+		data->altitude += data->altitude > 500 ? 0 : 1;
 	else if (key == 27)
-		data->altitude -= 10;
+		data->altitude += data->altitude < -500 ? 0 : -1;
+	else if (key == 47)
+		data->scale += data->scale > 200 ? 0 : 1;
+	else if (key == 43)
+		data->scale += data->scale < 5 ? 0 : -1;
+
 	return (1);
 }
 
